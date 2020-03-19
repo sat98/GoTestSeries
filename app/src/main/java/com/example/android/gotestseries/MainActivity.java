ad40bb7936a8871a8665e6bfb05d4dfd9c2ed865
sat98 <satirthabasakfb@gmail.com>
2020-03-19 23:25:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/gotestseries/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.android.gotestseries;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.loadUrl(" https://gotestseries.com/");
+
+
+    }
+}
